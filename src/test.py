871cc802b712179c7f879e4b54ca273cd4427939
Saksham Goel <szg0112@auburn.edu>
2021-10-29 10:32:18 -0500

    added updated genre-count

--- src/test.py
+++ src/test.py
@@ -1,4 +1,20 @@
 import json, os
 
-with open('datasets/artist-to-genre.json') as f:
-  data = json.load(f)
+def main():
+  with open(r'C:\Users\goels\Desktop\fall 2021\COMP 5970\final project\github\datasets\genre-count.json') as f:
+    data = json.load(f)
+
+
+  #new_data = {k: v for k, v in sorted(data.items(), key = lambda item: item[1], reverse = True)}
+
+  x = 0
+  for key, value in data.items():
+    print(key, ': ', value)
+    x += 1
+
+    if x > 20:
+      break
+
+
+
+main()
